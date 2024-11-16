f38326548a55e86c5ac45d823ceba2b522d4ffaa
Sunnatilla <sunnatilla.ismailov@gmail.com>
2024-11-16 02:16:08 +0200

    create submit issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
-const page = () => {
+const IssuesPage = () => {
   return (
     <div>
-      <Button>New issues</Button>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
     </div>
   );
 };
 
-export default page;
+export default IssuesPage;
